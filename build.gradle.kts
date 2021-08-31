0c9d61fca927d2cee0787379965317b7122ea7a1
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-08-31 22:46:22 +0100

    add acf and adventure dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,17 @@ repositories {
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://oss.sonatype.org/content/repositories/snapshots")
     maven("https://oss.sonatype.org/content/repositories/central")
+    maven("https://repo.aikar.co/content/groups/aikar/")
+    maven("https://papermc.io/repo/repository/maven-public/")
 }
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
     implementation(kotlin("stdlib"))
 
+    implementation("net.kyori:adventure-api:4.8.1")
+    implementation("net.kyori:adventure-platform-bukkit:4.0.0-SNAPSHOT")
+    implementation("net.kyori:adventure-text-minimessage:4.1.0-SNAPSHOT")
+
+    implementation("co.aikar:acf-paper:0.5.0-SNAPSHOT")
 }
