f54ce75779913bc9625583828cb5171936f3a7cd
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-08-31 22:04:50 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.30"
+}
+
+group = "me.bristermitten"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
