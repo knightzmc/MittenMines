971a461153964d73a83d50786f96e80b4caa5e71
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-09-01 20:51:44 +0100

    make things compile and work #34

--- core/src/main/kotlin/me/bristermitten/mittenmines/chat/ChatModule.kt
+++ core/src/main/kotlin/me/bristermitten/mittenmines/chat/ChatModule.kt
@@ -5,10 +5,15 @@ import dev.misfitlabs.kotlinguice4.multibindings.KotlinMultibinder
 import me.bristermitten.mittenmines.chat.hook.ChatHook
 import me.bristermitten.mittenmines.chat.hook.HexColorFixerHook
 import me.bristermitten.mittenmines.chat.hook.PAPIChatHook
+import me.bristermitten.mittenmines.module.MinesModule
+import net.kyori.adventure.platform.bukkit.BukkitAudiences
 
-class ChatModule : KotlinModule() {
+object ChatModule : MinesModule, KotlinModule() {
+    override val id = "chat"
+    override val guiceModule = this
     override fun configure() {
         bind<ChatFormatter>().to<MiniMessageFormatter>()
+        bind<BukkitAudiences>().toProvider<AdventureAudienceProvider>()
 
         val multibinder = KotlinMultibinder.newSetBinder<ChatHook>(binder())
         multibinder.addBinding().toInstance(PAPIChatHook)
