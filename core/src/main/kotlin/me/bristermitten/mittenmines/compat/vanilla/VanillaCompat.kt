2f6a1dd9f48dd37cf22c72175ccc6261ca28cb59
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-09-01 21:55:25 +0100

    make blockdata work for specific versions #2

--- core/src/main/kotlin/me/bristermitten/mittenmines/compat/vanilla/VanillaCompat.kt
+++ core/src/main/kotlin/me/bristermitten/mittenmines/compat/vanilla/VanillaCompat.kt
@@ -2,9 +2,11 @@ package me.bristermitten.mittenmines.compat.vanilla
 
 import me.bristermitten.mittenmines.compat.RegionSelection
 import me.bristermitten.mittenmines.compat.VersionCompat
+import me.bristermitten.mittenmines.modernworldedit.ModernBlockDataFactory
 
 object VanillaCompat : VersionCompat {
     override val blockPlacer = BukkitBlockPlacer
     override val regionSelection: RegionSelection
         get() = TODO("Not yet implemented")
+    override val blockDataFactory = ModernBlockDataFactory
 }
