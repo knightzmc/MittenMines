2f62e011acc23636dd0b3442d782bcdd7ae8f415
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-09-01 21:48:40 +0100

    make modern module depend on 1.13 spigot

--- modern-worldedit/build.gradle.kts
+++ modern-worldedit/build.gradle.kts
@@ -3,8 +3,11 @@ repositories {
 }
 
 dependencies {
-    compileOnly(project(":api"))
+    compileOnly(project(":api")) {
+        exclude("org.spigotmc")
+    }
     compileOnly(project(":compat"))
+    compileOnly("org.spigotmc:spigot-api:1.13.1-R0.1-SNAPSHOT")
     compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.0.0")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.0") {
         exclude("com.sk89q", "worldedit")
