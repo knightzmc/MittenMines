aa6450e076e0755891b38982d5765200eec34352
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-09-10 23:39:04 +0100

    fix STUPID kotlinx again

--- common/src/main/kotlin/me/bristermitten/mittenmines/block/BlockData.kt
+++ common/src/main/kotlin/me/bristermitten/mittenmines/block/BlockData.kt
@@ -1,12 +1,17 @@
 package me.bristermitten.mittenmines.block
 
 import kotlinx.serialization.Contextual
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
 @Serializable
-data class BlockData(val type: Material, val data: Byte? = null, @Contextual val bukkitData: Any? = null) {
+data class BlockData(
+    @SerialName("blockType") val type: Material,
+    val data: Byte? = null,
+    @Contextual val bukkitData: Any? = null,
+) {
     fun toItemStack(): ItemStack {
         return ItemStack(type, 1, data?.toShort() ?: 0)
     }
