42260da5afc18c3ddf155f554eaf830644826644
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-09-11 23:37:45 +0100

    make MinePublicity properly serializable

--- common/src/main/kotlin/me/bristermitten/mittenmines/mine/permission/MinePublicity.kt
+++ common/src/main/kotlin/me/bristermitten/mittenmines/mine/permission/MinePublicity.kt
@@ -1,6 +1,7 @@
 package me.bristermitten.mittenmines.mine.permission
 
 import kotlinx.serialization.Serializable
+import me.bristermitten.mittenmines.serialization.kotlinx.UUIDSerializer
 import java.util.*
 
 @Serializable
@@ -8,18 +9,22 @@ sealed class MinePublicity {
     abstract fun canEnter(uuid: UUID): Boolean
 }
 
+@Serializable
 object OpenPublicity : MinePublicity() {
     override fun canEnter(uuid: UUID) = true
 }
 
+@Serializable
 object ClosedPublicity : MinePublicity() {
     override fun canEnter(uuid: UUID) = false
 }
 
-data class WhitelistPublicity(val allowed: Set<UUID>) : MinePublicity() {
+@Serializable
+data class WhitelistPublicity(val allowed: Set<@Serializable(with = UUIDSerializer::class) UUID>) : MinePublicity() {
     override fun canEnter(uuid: UUID) = uuid in allowed
 }
 
-data class BlacklistPublicity(val disallowed: Set<UUID>) : MinePublicity() {
+@Serializable
+data class BlacklistPublicity(val disallowed: Set<@Serializable(with = UUIDSerializer::class) UUID>) : MinePublicity() {
     override fun canEnter(uuid: UUID) = uuid !in disallowed
 }
