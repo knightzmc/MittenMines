4f1af9a531e576962bc212cd6f18f94a796fb9e8
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-09-11 17:36:00 +0100

    make ConstantBlockPattern.kt not inline to prevent ktx errors

--- common/src/main/kotlin/me/bristermitten/mittenmines/block/ConstantBlockPattern.kt
+++ common/src/main/kotlin/me/bristermitten/mittenmines/block/ConstantBlockPattern.kt
@@ -3,8 +3,7 @@ package me.bristermitten.mittenmines.block
 import kotlinx.serialization.Serializable
 
 @Serializable
-@JvmInline
-value class ConstantBlockPattern(val data: BlockData) : BlockPattern {
+data class ConstantBlockPattern(val data: BlockData) : BlockPattern {
     override fun createData() = data
     override fun getItems() = listOf(data)
 }
